2d6f3334e842eef3fcb16ba33cf2ff5ae1bd1962
dr. kraemer <jkraemer@everett.k12.ma.us>
2026-02-25 09:05:59 -0500

    Add missing uno_io file.

---
+++ src/uno_io.cc
@@ -0,0 +1,26 @@
+#include "uno_io.h"
+
+void uno_io(Hardware hardware) {
+
+  // digit display
+  assign(hardware, PF::DD_A, 6);
+  assign(hardware, PF::DD_B, 2);
+  assign(hardware, PF::DD_C, 10);
+  assign(hardware, PF::DD_D, 12);
+  assign(hardware, PF::DD_E, 13);
+  assign(hardware, PF::DD_F, 5);
+  assign(hardware, PF::DD_G, 9);
+  assign(hardware, PF::DD_P, 11);
+  assign(hardware, PF::DD_1, 7);
+  assign(hardware, PF::DD_2, 4);
+  assign(hardware, PF::DD_3, 3);
+  assign(hardware, PF::DD_4, 8);
+
+  // dial encoder
+  
+  assign(hardware, PF::CLOCK, 17);   // A3
+  assign(hardware, PF::DATA, 16);    // A2
+  assign(hardware, PF::SWITCH, 15);  // A1
+}
+
+
